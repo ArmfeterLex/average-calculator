3ae6a2095817ec99ad9a5dff4ec5c009abeae580
ArmfeterLex <alexromanenkobeats@yandex.ru>
2026-03-01 13:43:25 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите два любых числа");
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        double c = a + b;
+        double d = Math.Round(c/2, 2);
+        Console.WriteLine("Среднее арифметическое этих чисел {0:f2}", d);
+        Console.ReadKey();
+    }
+}
